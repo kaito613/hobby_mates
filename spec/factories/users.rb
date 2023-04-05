83b33e7de7c78c2155384fb6fb4c8cd77e633e87
kaito613 <perfume.mth.pc@gmail.com>
2023-04-05 17:16:45 +0900

    Devise RSpec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    name { "テスト太郎" }
+    sequence :email do |n|
+        "test#{n}@example.com"
+    end
+    password { "Password2023" }
+    password_confirmation { "Password2023" }
   end
 end
