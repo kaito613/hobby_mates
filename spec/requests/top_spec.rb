ffedeeff35894f35d5977f5ac8fa7060b026e7e3
kaito613 <perfume.mth.pc@gmail.com>
2023-04-28 18:36:04 +0900

    css_framework_Bulmaを導入した。トップページのみ整えた

--- spec/requests/top_spec.rb
+++ spec/requests/top_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "Tops", type: :request do
   
+  describe "home" do
+    before do
+      create(:board)
+    end
+    context "トップページにアクセスした場合" do
+      it "リクエストが成功する" do
+        get root_path
+        expect(response).to have_http_status(:success)
+      end
+      it "変数に掲示板が全て降順で格納されているか" do
+        get root_path
+        boards = controller.instance_variable_get("@boards")
+        expect(boards).to eq Board.all.order(created_at: :desc)
+      end
+    end
+  end
 end
