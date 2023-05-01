2336db45941c9120545119333066509802d47ba7
kaito613 <perfume.mth.pc@gmail.com>
2023-05-01 10:21:54 +0900

    プロフィールページを整えた

--- spec/requests/top_spec.rb
+++ spec/requests/top_spec.rb
@@ -4,17 +4,18 @@ RSpec.describe "Tops", type: :request do
   
   describe "home" do
     before do
-      create(:board)
+      create_list(:board, 10)
     end
     context "トップページにアクセスした場合" do
       it "リクエストが成功する" do
         get root_path
         expect(response).to have_http_status(:success)
       end
-      it "変数に掲示板が全て降順で格納されているか" do
+      it "変数に掲示板が格納されているか" do
+        # 5件が大切ではない、格納されていることが大切である。
         get root_path
         boards = controller.instance_variable_get("@boards")
-        expect(boards).to eq Board.all.order(created_at: :desc)
+        expect(boards).to eq Board.all.page(1).per(5).order(created_at: :desc)
       end
     end
   end
